545139d64bdd6a2e427168b88c1ec4c946951bbe
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-04-20 15:40:43 +0700

    memasukkan ukuran array

--- mergesort.cpp
+++ mergesort.cpp
@@ -3,3 +3,32 @@ using namespace std;
 
 
 int arr[20], b[20];
+
+int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+    cout << "\n---------------------------------" << endl;
+    cout << "\nInputkan isi element array" << endl;
+    cout << "\n---------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "array index ke- " << i << " : ";
+        cin >> arr[i];
+    } 
+}
